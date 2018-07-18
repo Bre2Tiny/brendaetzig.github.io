50aa31fd7b5672adfa0548cf7ac286b864dbfd51
Student <student@MacBook-Pro-2.local>
2018-07-18 16:11:51 -0400

    Added maps api

---
+++ maps.js
@@ -0,0 +1,74 @@
+
+var ourLoc;
+var view;
+var map;
+
+function init() {
+	// Initalize things here
+	ourLoc = ol.proj.fromLonLat([41.043316, 28.862457]);
+
+	view = new ol.View({
+		center: ourLoc,
+		zoom: 6
+	});
+
+	map = new ol.Map({
+		target: 'map',
+		layers: [
+		  new ol.layer.Tile({
+		    source: new ol.source.OSM()
+		  })
+		],
+		// Note from the View Animation website:
+		// Improve user experience by loading tiles while animating. Will make
+		// animations stutter on mobile or slow devices.
+		loadTilesWhileAnimating: true,
+		view: view
+	});
+}
+
+function panHome() {
+	view.animate({
+		center: ourLoc, // "Home" Location
+		duration: 2000  // Two seconds
+	});
+}
+
+function panToCountry(){
+	var countryName = document.getElementById("country-name").value;
+	var lon = 0.0;
+	var lat = 0.0;
+  var query = "https://restcountries.eu/rest/v2/name/"+countryName
+	query = query.replace(/ /g, "%20")
+
+	alert(query);
+
+	// Step 1: Let's start by making an HTTP GET request:
+	var countryRequest = new XMLHttpRequest();
+	countryRequest.open('GET', query, false);
+	countryRequest.send();
+
+	if(countryRequest.readyState !=4  ||  countryRequest.status != 200 || countryRequest.response === ""){
+    window.console.error("Request had an Error!");
+		return;
+	}
+
+	alert("Ready State " + countryRequest.readyState);
+	alert("Status " + countryRequest.status);
+	alert("Response" + countryRequest.responseText);
+
+	var countryInformation = JSON.parse(countryRequest.responseText);
+
+	lat = countryInformation[0].latlng[0];
+	lon = countryInformation[0].latlng[1];
+
+	alert(countryName + ": lon " + lon + "& lat " + lat);
+
+var location = ol.proj.fromLonLat([lon, lat]);
+	view.animate({
+		center: location, // Location
+		duration: 2000  // Two seconds
+	});
+}
+
+window.onload = init;
